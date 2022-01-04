bde2b735472a38a7f3502a051c05b76e1fec2663
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-04 14:57:13 +0000

    Migration file generated, ready to update the database!

--- IamtuseTechMakeEasyWeb/Data/Migrations/20220104145625_ModifyingLengthAspnetusersCustomFields.Designer.cs
+++ IamtuseTechMakeEasyWeb/Data/Migrations/20220104145625_ModifyingLengthAspnetusersCustomFields.Designer.cs
@@ -4,14 +4,16 @@ using IamtuseTechMakeEasyWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IamtuseTechMakeEasyWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220104145625_ModifyingLengthAspnetusersCustomFields")]
+    partial class ModifyingLengthAspnetusersCustomFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
