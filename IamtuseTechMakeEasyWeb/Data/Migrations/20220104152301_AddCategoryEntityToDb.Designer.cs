91f5c1332decf014b92b01e429e0e9c56fea04cb
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-04 15:24:29 +0000

    Added migration for the Category entity, to stored all category content in the database!

--- IamtuseTechMakeEasyWeb/Data/Migrations/20220104152301_AddCategoryEntityToDb.Designer.cs
+++ IamtuseTechMakeEasyWeb/Data/Migrations/20220104152301_AddCategoryEntityToDb.Designer.cs
@@ -4,14 +4,16 @@ using IamtuseTechMakeEasyWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IamtuseTechMakeEasyWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220104152301_AddCategoryEntityToDb")]
+    partial class AddCategoryEntityToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IamtuseTechMakeEasyWeb/Data/Migrations/20220104152301_AddCategoryEntityToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IamtuseTechMakeEasyWeb.Data.Migrations
+{
+    public partial class AddCategoryEntityToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ThumbnailImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
