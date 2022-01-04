38482c45f69356a9fd25deb7ec8d4f806db06d03
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-04 14:55:59 +0000

    ModifyLengthAspnetuserCustomFields

--- IamtuseTechMakeEasyWeb/Data/ApplicationDbContext.cs
+++ IamtuseTechMakeEasyWeb/Data/ApplicationDbContext.cs
@@ -3,16 +3,22 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace IamtuseTechMakeEasyWeb.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(maximumLength:250)]
         public string FirstName { get; set; }
+        [StringLength(maximumLength: 250)]
         public string LastName { get; set; }
+        [StringLength(maximumLength: 250)]
         public string Address1 { get; set; }
+        [StringLength(maximumLength: 250)]
         public string Address2 { get; set; }
+        [StringLength(maximumLength: 50)]
         public string PostCode { get; set; }
     }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
