197eb3f2757df0b17dee23f4e1d5a10006fd47b8
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-04 15:21:00 +0000

    Added the Properties for the Category Entity

--- IamtuseTechMakeEasyWeb/Entities/Category.cs
+++ IamtuseTechMakeEasyWeb/Entities/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace IamtuseTechMakeEasyWeb.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IamtuseTechMakeEasyWeb.Entities
 {
     public class Category
     {
+        public int Id { get; set; }
+        [Required, StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+        [Required]
+        public string ThumbnailImagePath { get; set; }
+        [StringLength(255)]
+        public string Description { get; set; }
     }
 }
