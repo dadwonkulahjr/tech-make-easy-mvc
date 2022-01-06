b8d894648f8e9a8ca6b8c35e595d8c1e9a36a658
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-06 03:12:38 +0000

    Added the CategoryItem Entity with it's properties and Foreign Keys

--- IamtuseTechMakeEasyWeb/Entities/CategoryItem.cs
+++ IamtuseTechMakeEasyWeb/Entities/CategoryItem.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IamtuseTechMakeEasyWeb.Entities
 {
     public class CategoryItem
     {
+        public int Id { get; set; }
+        [StringLength(200, MinimumLength =2)]
+        public string Title { get; set; }
+        [Column(TypeName ="date"), Required]
+
+        public DateTime? DateTimeItemReleased { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public int CategoryId { get; set; }
+
     }
 }
