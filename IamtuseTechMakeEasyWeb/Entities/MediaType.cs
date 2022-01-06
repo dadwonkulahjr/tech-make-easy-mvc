8549611ea0caaa14daebbd1264c23fffe2b08c94
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-06 03:24:47 +0000

    Added the MediaType entity Properties, Data Annotations and Foreign Key Constraint

--- IamtuseTechMakeEasyWeb/Entities/MediaType.cs
+++ IamtuseTechMakeEasyWeb/Entities/MediaType.cs
@@ -1,6 +1,18 @@
-﻿namespace IamtuseTechMakeEasyWeb.Entities
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IamtuseTechMakeEasyWeb.Entities
 {
     public class MediaType
     {
+        public int Id { get; set; }
+        [Required, StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+        [StringLength(255), Required]
+        public string ThumbnailImagePath { get; set; }
+
+        [ForeignKey("MediaTypeId")]
+        public virtual ICollection<CategoryItem> CategoryItems { get; set; }
     }
 }
