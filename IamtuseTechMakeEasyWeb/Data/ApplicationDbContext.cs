80f8860677948f2175f42b9ff8c37686e8de1b79
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-06 03:39:43 +0000

    Added the Foreign Key Constraint relationship between the UserCategory and the Aspnetusers table

--- IamtuseTechMakeEasyWeb/Data/ApplicationDbContext.cs
+++ IamtuseTechMakeEasyWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace IamtuseTechMakeEasyWeb.Data
@@ -21,6 +22,8 @@ namespace IamtuseTechMakeEasyWeb.Data
         public string Address2 { get; set; }
         [StringLength(maximumLength: 50)]
         public string PostCode { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserCategory> UserCategories { get; set; }
     }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
