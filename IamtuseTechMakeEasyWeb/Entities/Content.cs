8a1d778b5d7343a6a8cd3f43b26482b90e2da2f2
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-06 03:33:10 +0000

    Added the Content Properties, Data Annotations and Foreign Key Constraint

--- IamtuseTechMakeEasyWeb/Entities/Content.cs
+++ IamtuseTechMakeEasyWeb/Entities/Content.cs
@@ -1,6 +1,20 @@
-﻿namespace IamtuseTechMakeEasyWeb.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IamtuseTechMakeEasyWeb.Entities
 {
     public class Content
     {
+        public int Id { get; set; }
+        [Required, StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+        [StringLength(255, MinimumLength = 5)]
+        public string HtmlContent { get; set; }
+        [StringLength(255, MinimumLength = 5)]
+        public string VideoLink { get; set; }
+
+        //Navigation Properties
+        //Foreign Key Constraint
+
+        public virtual CategoryItem CategoryItem { get; set; }
     }
 }
