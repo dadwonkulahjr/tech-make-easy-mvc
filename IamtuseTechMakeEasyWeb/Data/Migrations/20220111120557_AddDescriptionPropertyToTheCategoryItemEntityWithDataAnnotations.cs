5e8de9f3716e97edfd613c95ce232254791e85e1
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-11 12:07:02 +0000

    Migration to add the CategoryItem Description Property to the database!

---
+++ IamtuseTechMakeEasyWeb/Data/Migrations/20220111120557_AddDescriptionPropertyToTheCategoryItemEntityWithDataAnnotations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IamtuseTechMakeEasyWeb.Data.Migrations
+{
+    public partial class AddDescriptionPropertyToTheCategoryItemEntityWithDataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CategoryItems",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CategoryItems");
+        }
+    }
+}
