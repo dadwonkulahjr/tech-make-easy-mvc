135f28caf69f79bac3048e0133fb3a501120a6a3
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-11 14:08:14 +0000

    Added the logic to covert any type which has dropdownList on the UI

--- IamtuseTechMakeEasyWeb/Extensions/ConvertExtensions.cs
+++ IamtuseTechMakeEasyWeb/Extensions/ConvertExtensions.cs
@@ -1,6 +1,20 @@
-﻿namespace IamtuseTechMakeEasyWeb.Extensions
+﻿using IamtuseTechMakeEasyWeb.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IamtuseTechMakeEasyWeb.Extensions
 {
-    public class ConvertExtensions
+    public static class ConvertExtensions
     {
+        public static List<SelectListItem> ConvertToSelectListItem<T>(this IEnumerable<T> collection, int selectedValue) where T : IPrimaryProperties
+        {
+            return collection.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString(),
+                Selected = (x.Id == selectedValue)
+            }).ToList();
+        }
     }
 }
