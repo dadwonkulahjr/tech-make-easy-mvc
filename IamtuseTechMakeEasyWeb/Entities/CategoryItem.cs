cfdefd9311e905f7edb243bd2db0f99f018826e0
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-01-18 15:51:37 +0000

    Added the logic for the specified date

--- IamtuseTechMakeEasyWeb/Entities/CategoryItem.cs
+++ IamtuseTechMakeEasyWeb/Entities/CategoryItem.cs
@@ -8,12 +8,23 @@ namespace IamtuseTechMakeEasyWeb.Entities
 {
     public class CategoryItem
     {
+        private DateTime _releasedDate = DateTime.MinValue;
         public int Id { get; set; }
         [StringLength(200, MinimumLength =2)]
         public string Title { get; set; }
-        [Column(TypeName ="date"), Required, DisplayFormat(DataFormatString ="0:yyyy-MM-DD")]
+        [Column(TypeName ="date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 
-        public DateTime DateTimeItemReleased { get; set; }
+        public DateTime DateTimeItemReleased
+        {
+            get
+            {
+               return (_releasedDate == DateTime.MinValue) ? DateTime.Now : _releasedDate;
+            }
+            set
+            {
+                _releasedDate = value;
+            }
+        }
         [StringLength(255)]
 
         public string Description { get; set; }
