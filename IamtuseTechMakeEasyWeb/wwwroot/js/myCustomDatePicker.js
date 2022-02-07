3c261384c34046933fa2377aa977684e76a40cb8
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-02-07 16:35:55 +0000

    Added a restriction on the date field on the UI, the user can only selected two months ahead.

--- IamtuseTechMakeEasyWeb/wwwroot/js/myCustomDatePicker.js
+++ IamtuseTechMakeEasyWeb/wwwroot/js/myCustomDatePicker.js
@@ -1,7 +1,19 @@
 ﻿$(document).ready(function () {
     $('.myCustomDate').datepicker(
         {
-            dateFormat: "yy-mm-dd"
+            dateFormat: "yy-mm-dd",
+            minDate: new Date(),
+            maxDate: addSubstractMonth(new Date(), 2)
         }
     );
+
+
+    function addSubstractMonth(date, numMonths) {
+        var month = date.getMonth();
+
+        var milliSeconds = new Date(date).setMonth(month + numMonths);
+        return new Date(milliSeconds);
+
+
+    }
 });
