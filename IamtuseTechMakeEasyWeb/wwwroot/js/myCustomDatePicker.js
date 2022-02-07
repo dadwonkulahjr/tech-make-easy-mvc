8279b7f55811aa1bbaef82d87c3563ba1de2ac5f
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-02-07 16:40:13 +0000

    Removed the date functions from the default datepicker js file.

--- IamtuseTechMakeEasyWeb/wwwroot/js/myCustomDatePicker.js
+++ IamtuseTechMakeEasyWeb/wwwroot/js/myCustomDatePicker.js
@@ -6,14 +6,4 @@
             maxDate: addSubstractMonth(new Date(), 2)
         }
     );
-
-
-    function addSubstractMonth(date, numMonths) {
-        var month = date.getMonth();
-
-        var milliSeconds = new Date(date).setMonth(month + numMonths);
-        return new Date(milliSeconds);
-
-
-    }
 });
