1e695fa324ef8783711e0697a2bb79b1344ceeee
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-02-23 16:22:41 +0000

    Added security for only admin users that can access this view using the Authorize attribute

--- IamtuseTechMakeEasyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ IamtuseTechMakeEasyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IamtuseTechMakeEasyWeb.Data;
 using IamtuseTechMakeEasyWeb.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IamtuseTechMakeEasyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _context;
