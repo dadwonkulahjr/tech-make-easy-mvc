f448ff408bfc8575d6eb6d6c37587f1dc9cce75a
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-02-23 15:42:14 +0000

    Added data annotation for the Category Type

--- IamtuseTechMakeEasyWeb/Entities/Category.cs
+++ IamtuseTechMakeEasyWeb/Entities/Category.cs
@@ -9,7 +9,7 @@ namespace IamtuseTechMakeEasyWeb.Entities
         public int Id { get; set; }
         [Required, StringLength(200, MinimumLength = 2)]
         public string Title { get; set; }
-        [Required]
+        [Required, Display(Name ="Thumbnail Image")]
         public string ThumbnailImagePath { get; set; }
         [StringLength(255)]
         public string Description { get; set; }
