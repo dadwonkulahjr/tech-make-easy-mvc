39cbb191c51a95e0b4bf9499f35acebb97eeb1dc
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-02-25 16:54:05 +0000

    Added the LoginModel to capture the user login info on the UI.

---
+++ IamtuseTechMakeEasyWeb/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IamtuseTechMakeEasyWeb.Models
+{
+    public class LoginModel
+    {
+
+        [Required, EmailAddress, StringLength(100, MinimumLength = 2)]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password), StringLength(100, MinimumLength = 2)]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me?")]
+        public bool RememberMe { get; set; }
+
+        public string LoginInvalid { get; set; }
+    }
+}
