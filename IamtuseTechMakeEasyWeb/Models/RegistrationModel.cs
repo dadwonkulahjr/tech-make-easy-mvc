5c357775c23ac9546dd0bb18ddfc212c6f919006
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-03-02 16:47:28 +0000

    Added the Registration Model to caputre all register user in the system.

---
+++ IamtuseTechMakeEasyWeb/Models/RegistrationModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IamtuseTechMakeEasyWeb.Models
+{
+    public class RegistrationModel
+    {
+        [EmailAddress, Required, Display(Name ="User Name")]
+        public string Email { get; set; }
+        [DataType(DataType.Password), Required]
+        public string Password { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "Password and confirm password do not match."), Required,
+            Display(Name ="Confirm Password"), DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        [Display(Name ="Post Code")]
+        public string PostCode { get; set; }
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+        [Display(Name ="Acccept User Agreement")]
+        public bool AcceptUserAgreement { get; set; }
+
+        public string RegistrationInvalid { get; set; }
+
+
+        [Display(Name ="Phone Number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
