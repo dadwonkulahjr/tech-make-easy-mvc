d56e54f6ec11dfb5e5a4399e22013585baddf921
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-03-08 14:13:30 +0000

    Added a reusable custombootstrapalert to used accross various aspect of the webapp that needs alert.

---
+++ IamtuseTechMakeEasyWeb/wwwroot/js/customBootstrapAlert.js
@@ -0,0 +1,17 @@
+﻿function presentClosableBootstrapAlert(placeholderElemId, alertType, alertHeading, alertMessage) {
+
+    if (alertType == '') {
+        alertType = 'info';
+    }
+
+    var htmlToDisplay = `<div class="alert alert-` + alertType + ` alert-dismissible fade show" role="alert">
+                          <strong>`+ alertHeading + `</strong><br/>` + alertMessage + `
+                          <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+
+    $(placeholderElemId).html(htmlToDisplay);
+}
+
+function closeAlert(placeholderElemId) {
+    $(placeholderElemId).html(htmlToDisplay);
+}
