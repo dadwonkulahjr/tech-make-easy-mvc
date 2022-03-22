84b8f7ed5beb23d5c2e97f6c82aace080ac86d14
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-03-22 17:30:03 +0000

    Allow the Category class implement the IPrimaryProperties to retrived the title and Id an a specific Category in the View.

--- IamtuseTechMakeEasyWeb/Entities/Category.cs
+++ IamtuseTechMakeEasyWeb/Entities/Category.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using IamtuseTechMakeEasyWeb.Interfaces;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IamtuseTechMakeEasyWeb.Entities
 {
-    public class Category
+    public class Category : IPrimaryProperties
     {
         public int Id { get; set; }
         [Required, StringLength(200, MinimumLength = 2)]
