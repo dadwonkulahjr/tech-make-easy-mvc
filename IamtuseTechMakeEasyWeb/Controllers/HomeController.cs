b8fc52beb6adcae66a6e9492398c611d027874d6
dadwonkulahjr <dad.wonkulah@comaxict.com>
2022-03-22 17:28:27 +0000

    Added the logic to retrived all category and categoryItem in a group on the UI using a card.

--- IamtuseTechMakeEasyWeb/Controllers/HomeController.cs
+++ IamtuseTechMakeEasyWeb/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using IamtuseTechMakeEasyWeb.Models;
+﻿using IamtuseTechMakeEasyWeb.Data;
+using IamtuseTechMakeEasyWeb.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +15,78 @@ namespace IamtuseTechMakeEasyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            ApplicationDbContext applicationDbContext,
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _applicationDbContext = applicationDbContext;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        private  IEnumerable<GroupedCategoryItemsByCategoryModel> GetGroupedCategoryItemsByCategories(
+            IEnumerable<CategoryItemDetailsModel> categoryItemDetailsModels)
+        {
+            return from categoryItem in categoryItemDetailsModels
+                   group categoryItem by categoryItem.CategoryId into g
+                   select new GroupedCategoryItemsByCategoryModel
+                   {
+                       Id = g.Key,
+                       Title = g.Select(x => x.CategoryTitle).FirstOrDefault(),
+                       Items = g
+                   };
+                         
+        }
+        private async Task<IEnumerable<CategoryItemDetailsModel>> GetCategoryItemsDetailsForUser(string userId)
+        {
+            return await (from catItem in _applicationDbContext.CategoryItems
+                          join category in _applicationDbContext.Categories
+                          on catItem.CategoryId equals category.Id
+                          join content in _applicationDbContext.Contents
+                          on catItem.Id equals content.CategoryItem.Id
+                          join userCat in _applicationDbContext.UserCategories
+                          on category.Id equals userCat.CategoryId
+                          join mediaType in _applicationDbContext.MediaTypes
+                          on catItem.MediaTypeId equals mediaType.Id
+                          where userCat.UserId == userId
+                          select new CategoryItemDetailsModel
+                          {
+                              CategoryId = category.Id,
+                              CategoryTitle = category.Title,
+                              CategoryItemId = catItem.Id,
+                              CategoryItemTitle = catItem.Title,
+                              CategoryItemDescription = catItem.Description,
+                              MediaImagePath = mediaType.ThumbnailImagePath
+                          }).ToListAsync();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<CategoryItemDetailsModel> categoryItemDetailsModels = null;
+            IEnumerable<GroupedCategoryItemsByCategoryModel> groupedCategoriesModels = null;
+
+            CategoryDetailsModel model = new();
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                if (user != null)
+                {
+                    categoryItemDetailsModels = await GetCategoryItemsDetailsForUser(user.Id);
+                    groupedCategoriesModels = GetGroupedCategoryItemsByCategories(categoryItemDetailsModels);
+
+                    model.GroupedCategoryItemsByCategoryModels = groupedCategoriesModels;
+                }
+
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
